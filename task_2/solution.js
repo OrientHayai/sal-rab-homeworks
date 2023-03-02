8824216341495a17011bb85c5662a1ef290422b5
Orient <skoralex@yandex.ru>
2023-03-03 01:47:24 +0300

    change3

--- task_2/solution.js
+++ task_2/solution.js
@@ -18,7 +18,7 @@ function calcShipping(sum, min, shipping) {
     let shippingSum;
     if(productsSum == 0 || productsSum >= freeShippingMinSum){
         shippingSum = 0;
-    }else if(productsSum > 0 && productsSum < freeShippingMinSum){
+    }else{
         shippingSum = shippingPrice;
     }
 
